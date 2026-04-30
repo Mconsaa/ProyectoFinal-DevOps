edc81d6f6c0fd5c3351a64dd05eac5fdd245f5b5
Mauro Condori <mauroguconsa@gmail.com>
2026-04-30 15:46:36 -0400

    correccion tests

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 import os
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 from app import app as flask_app
 
