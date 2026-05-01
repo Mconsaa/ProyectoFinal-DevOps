702e9ab447b9f5bf2209e934b37776dae3f05411
Mauro Condori <mauroguconsa@gmail.com>
2026-04-30 21:47:33 -0400

    correcion testpy

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-from app import app as flask_app
+from app.app import app as flask_app
 
 @pytest.fixture
 def client():
